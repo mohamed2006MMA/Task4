fff5e624dcd32ea1ad80ed2d22ea8ec6f9e94a95
mohamed adel <betngan614@gmail.com>
2024-08-24 18:33:32 +0200

    task 4

---
+++ cv.php
@@ -0,0 +1,38 @@
+<?php
+$cv =[
+            "name"  => "Mohamed Adel",
+            "age" => "32",
+            "education" => "BSc in Computer Science",
+            "experience" => ["flutter developer", "php back end"],
+            "about_me" => "Meet Mohamed, a dedicated computer science student with a passion for technology and innovation. Currently pursuing a degree in computer science, Mohamed is fascinated by programming, algorithms, and data structures. With a strong curiosity for learning and problem-solving, he is always eager to explore new areas of computing, from artificial intelligence to software development. Driven by a desire to make a meaningful impact in the tech industry, Mohamed is continuously honing his skills and expanding his knowledge base to become a proficient and versatile computer scientist.",
+            "skills" => [
+                "Programming languages: PHP, JavaScript, HTML, CSS",
+                "Databases: MySQL, PostgreSQL",
+                "Frameworks: Laravel, CodeIgniter",
+                "Tools: Git, Jira, Trello",
+                "Version control: SVN",
+                "IDEs: Visual Studio Code, Sublime Text",
+                "Operating systems: Windows, Linux",
+                "about_me" => "Meet Mohamed, a dedicated computer science student with a passion for technology and innovation. Currently pursuing a degree in computer science, Mohamed is fascinated by programming, algorithms, and data structures. With a strong curiosity for learning and problem-solving, he is always eager to explore new areas of computing, from artificial intelligence to software development. Driven by a desire to make a meaningful impact in the tech industry, Mohamed is continuously honing his skills and expanding his knowledge base to become a proficient and versatile computer scientist.", 
+           ],
+            "contact" => [
+                "Email" =>  "mohamedadel@example.com",
+                "Phone" => "+201234567890",
+                "Address" => "123 Main St, City, State, ZIP"
+            ],
+            "projects" =>  [
+                [   "from_date" => "2019", 
+                    "to_date" => "2021",
+                    "from_date2"=> "2022",
+                    "to_date2"=> "2023",
+                    "from_date3" => "2024",
+                    "to_date3"=> "present",
+                    "title3" => "IT-Consultant",
+                    "title2" => "Designer and author",
+                    "title" => "eines Web-Studios",
+                ],
+               
+]
+]
+    
+;
